d21be1fe6d477049a12cd1e65e417c93b9569adf
jongfeelkim-VIRNECT <jongfeel.kim@virnect.com>
2020-03-10 11:37:28 +0900

    Add APIKeyLoader from Application.persistanceDataPath Add method Base64Decode from wav file

--- Assets/Scripts/MicrophoneController.cs
+++ Assets/Scripts/MicrophoneController.cs
@@ -16,10 +16,13 @@ public class MicrophoneController : MonoBehaviour
     public StringEvent OnRecognizeSpeechProcessing;
     public StringEvent OnRecognizeSpeechFail;
 
+    public string APIKey { set; get; }
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        Debug.Log(APIKey);
         StartToSpeech();
     }
 
@@ -71,10 +74,35 @@ public class MicrophoneController : MonoBehaviour
                 waitForEndOfSpeech = 0;
                 Debug.Log("Microphone.End");
                 Microphone.End(microphoneDevice);
-                SavWav.Save("Recorded", audioSource.clip);
+                SavWav.Save("Recorded.wav", audioSource.clip);
                 audioSource.volume = 1.0f;
                 audioSource.Stop();
+
+                string wavFilePath = Path.Combine(Application.persistentDataPath, "Recorded.wav");
+                string base64decoded = GetBase64DecodeFromWavFile(wavFilePath);
+            }
+        }        
+    }
+
+    private string GetBase64DecodeFromWavFile(string wavFilePath)
+    {
+        OnRecognizeSpeechProcessing?.Invoke("Processing Google.Speech.API...");
+
+        byte[] wavByteArray = File.ReadAllBytes(wavFilePath);
+
+        // Remove empty space, decoded string "A"
+        string contents = Convert.ToBase64String(wavByteArray);
+        int lastIndex = 0;
+        for (int i = contents.Length - 1; i != 0; i--)
+        {
+            if (contents[i] != 'A')
+            {
+                lastIndex = i + 1;    // Prevent remove last index string
+                break;
             }
         }
+        string removedEmptyContents = contents.Substring(0, lastIndex);
+        //File.WriteAllText(Path.Combine(Application.persistentDataPath, "Base64Decoded.txt"), removedEmptyContents);
+        return removedEmptyContents;
     }
 }
