6daf9705fadc97917a430b00ff25a00a15d25b2f
jongfeelkim-VIRNECT <jongfeel.kim@virnect.com>
2020-03-11 14:40:19 +0900

    Rename file AudioContent => TTSResponseBody. Add translate json data classes

---
+++ Assets/Scripts/Translate/JsonData/TranslateRequestBody.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Translate.JsonData
+{
+    [Serializable]
+    class TranslateRequestBody
+    {
+        public List<string> contents;
+        public string mimeType;
+        public string sourceLanguageCode;
+        public string targetLanguageCode;
+    }
+}
---
+++ Assets/Scripts/Translate/JsonData/TranslatedText.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scripts.Translate.JsonData
+{
+    [Serializable]
+    class TranslatedText
+    {
+        public string translatedText;
+    }
+}
